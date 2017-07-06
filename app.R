836b0aad705964b20c9ab7171c1168aff8340a76
Romain Francois <romain@thinkr.fr>
2017-07-07 01:00:54 +0200

    update

--- app.R
+++ app.R
@@ -1,10 +1,11 @@
 library(shinydashboard)
 library(shiny)
 library(jsonlite)
+library(purrr)
 
 tweet <- function(id){
   url <- paste0( "https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F", id )
-  HTML( jsonlite::fromJSON(url)$html )
+  HTML( fromJSON(url)$html )
 }
 
 ui <- dashboardPage(
@@ -12,36 +13,59 @@ ui <- dashboardPage(
   dashboardSidebar(disable = TRUE),
   dashboardBody(
 
-    tags$head(tags$script('!function(d,s,id){var js,fjs=d.getElementsByTagName(s)    [0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");')),
-
-
+    # tags$head(tags$script('!function(d,s,id){var js,fjs=d.getElementsByTagName(s)    [0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");')),
 
     # Boxes need to be put in a row (or column)
     fluidRow(
 
-      valueBoxOutput("n_tweets"),
-      valueBoxOutput("n_screen_name")
+      valueBoxOutput("n_tweets", width = 2),
+      valueBoxOutput("n_screen_name", width = 2),
+      valueBoxOutput("n_hashtags", width = 2)
 
     ),
 
     fluidRow(
-      box(
-        title = "Most popular tweet (favorite count)",
-        uiOutput("most_popular_tweet")
+      tabBox( title = "Popular", id = "popular_tabbox", width = 4,
+        tabPanel( "Most Popular tweets",
+          uiOutput("most_popular_tweets")
+        ),
+        tabPanel( "Most Retweeted",
+          uiOutput("most_retweeted")
+        )
       ),
 
-      box(
-        title = "Most retweeted",
-        uiOutput("most_retweeted")
-      )
+      tabBox( title = "Users", id = "users_tabbox", width = 4,
+        tabPanel( "User", DT::dataTableOutput("users") ),
+        tabPanel( "Cited", DT::dataTableOutput("cited_users") ),
+        tabPanel( "Replied to", DT::dataTableOutput("replied_users") )
+      ),
 
+      tabBox( title = "Content", id = "content_tabbox", width = 4,
+        tabPanel( "Hashtags", DT::dataTableOutput("hashtags") ),
+        tabPanel( "Medias", DT::dataTableOutput("medias") )
+      )
     )
   )
 )
 
+user_data <- function( x ){
+  users <- tibble(
+    id   = str_split( x, " " ) %>% flatten_chr()
+  ) %>%
+    filter( !is.na(id) ) %>%
+    group_by(id) %>%
+    summarise( n = n() ) %>%
+    arrange( desc(n) )
+
+  lookups <- lookup_users(users$id)
+
+  res <- bind_cols( select(users, -id), lookups )
+  res
+}
+
 server <- function(input, output) {
 
-  tweets <- search_tweets( "#useR2017 #user2017", n = 18000,  include_rts = FALSE)
+  # tweets <- search_tweets( "#useR2017 #user2017", n = 18000,  include_rts = FALSE)
 
   n_tweets <- reactive({
     nrow(tweets)
@@ -51,12 +75,35 @@ server <- function(input, output) {
     length( unique(tweets$screen_name) )
   })
 
-  most_popular_tweet <- reactive({
-    tweets %>% top_n( n = 1, favorite_count ) %>% pull(status_id)
+  most_popular_tweets <- reactive({
+    tweets %>% top_n( n = 5, favorite_count ) %>% arrange( desc(favorite_count) ) %>% pull(status_id)
   })
 
   most_retweeted <- reactive({
-    tweets %>% top_n( n = 1, retweet_count ) %>% pull(status_id)
+    tweets %>% top_n( n = 5, retweet_count ) %>% arrange( desc(retweet_count) ) %>% pull(status_id)
+  })
+
+  users <- reactive( user_data( tweets$user_id ) )
+  cited <- reactive( user_data( tweets$mentions_user_id ) )
+  replied_users <- reactive( user_data( tweets$in_reply_to_status_user_id ) )
+
+  hashtags <- reactive({
+    tibble(
+      hashtag   = str_split( tweets$hashtags, " " ) %>% flatten_chr()
+    ) %>%
+      filter( !is.na(hashtag) ) %>%
+      group_by(hashtag) %>%
+      summarise( n = n() ) %>%
+      arrange( desc(n) )
+  })
+
+  medias <- reactive({
+    tweets %>%
+      filter( !is.na(media_url) ) %>%
+      select( status_id, user_id, media_url, favorite_count ) %>%
+      arrange( desc( favorite_count ) ) %>%
+      mutate( media = sprintf( '<img src="%s" width="100%%"/> ', media_url ) ) %>%
+      select( favorite_count, media )
   })
 
 
@@ -68,12 +115,36 @@ server <- function(input, output) {
     valueBox( "Users", n_screen_name(), icon = icon("user"), color = "orange" )
   })
 
-  output$most_popular_tweet <- renderUI({
-    tweet( most_popular_tweet() )
+  output$n_hashtags <- renderValueBox({
+    valueBox( "Hashtags", nrow(hashtags()), icon = icon("hashtag"), color = "blue" )
+  })
+
+  output$most_popular_tweets <- renderUI({
+    div( map( most_popular_tweets(), tweet ) )
   })
 
   output$most_retweeted <- renderUI({
-    tweet( most_retweeted() )
+    div( map( most_retweeted(), tweet ) )
+  })
+
+  output$users <- DT::renderDataTable({
+    DT::datatable( select( users(), name, n, followers_count ) )
+  })
+
+  output$cited_users <- DT::renderDataTable({
+    DT::datatable( select( cited(), name, n, followers_count ) )
+  })
+
+  output$replied_users <- DT::renderDataTable({
+    DT::datatable( select( replied_users(), name, n, followers_count ) )
+  })
+
+  output$hashtags <- DT::renderDataTable({
+    DT::datatable( hashtags() )
+  })
+
+  output$medias <- DT::renderDataTable({
+    DT::datatable( medias(), escape = FALSE, options = list( pageLength = 2) )
   })
 
 }
